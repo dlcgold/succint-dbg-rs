9c5c7225afd17e140c7abb1d6bef7ef230ddd4d5
dlcgold <dlcgold96@gmail.com>
2021-06-04 23:10:54 +0200

    fix doc

--- src/lib.rs
+++ src/lib.rs
@@ -16,26 +16,26 @@
 //! they are not they are not stored):
 //! ```rust,ignore
 //! last edge negative edge_index node_index label
-//! 1	 T	  false    0          0          $$$
-//! 1	 C	  false    1          1          CGA
-//! 1	 C	  false    2          2          $TA
-//! 0	 G	  false    3          3          GAC
-//! 1	 T	  false    4          3          GAC
-//! 1	 G	  true     5          4          TAC
-//! 1	 G	  false    6          5          GTC
-//! 0	 A	  false    7          6          ACG
-//! 1	 T	  false    8          6          ACG
-//! 1	 A	  true     9          7          TCG
-//! 1	 A	  false    10         8          $$T
-//! 1	 $	  false    11         9          ACT
-//! 1	 C	  false    12         10         CGT
+//! 1	 'T'  false    0          0          "$$$"
+//! 1	 'C'  false    1          1          "CGA"
+//! 1	 'C'  false    2          2          "$TA"
+//! 0	 'G'  false    3          3          "GAC"
+//! 1	 'T'  false    4          3          "GAC"
+//! 1	 'G'  true     5          4          "TAC"
+//! 1	 'G'  false    6          5          "GTC"
+//! 0	 'A'  false    7          6          "ACG"
+//! 1	 'T'  false    8          6          "ACG"
+//! 1	 'A'  true     9          7          "TCG"
+//! 1	 'A'  false    10         8          "$$T"
+//! 1	 '$'  false    11         9          "ACT"
+//! 1	 'C'  false    12         10         "CGT"
 //!
 //! F array:
-//! F($) = 0
-//! F(A) = 1
-//! F(C) = 3
-//! F(G) = 7
-//! F(T) = 10
+//! F('$') = 0
+//! F('A') = 1
+//! F('C') = 3
+//! F('G') = 7
+//! F('T') = 10
 //! ```
 //! And the dot of the graph is:
 //! <div>
